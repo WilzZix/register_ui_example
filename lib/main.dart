b04f85c38b5d8dc6051dc1a901971580569aa6ff
Your Name <you@example.com>
2022-11-19 18:57:14 +0500

    nothing

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:registration_example/flutter_icon.dart';
+import 'package:go_router/go_router.dart';
+import 'package:registration_example/register_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const MyHomePage();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'register',
+          builder: (BuildContext context, GoRouterState state) {
+            return const RegisterScreen();
+          },
+        ),
+      ],
+    ),
+  ],
+);
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: _router,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+
     );
   }
 }
@@ -32,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.all(20),
         children: [
           Image.asset(
             'assets/login.jpg',
@@ -45,12 +64,119 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           TextFormField(
             decoration: const InputDecoration(
+              labelText: 'Email',
               prefixIcon: Icon(
                 Icons.mail,
               ),
             ),
           ),
+          const SizedBox(
+            height: 8,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Password',
+              prefixIcon: Icon(
+                Icons.lock,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          SizedBox(
+            height: 40,
+            child: TextButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue)),
+              child: const Text(
+                'Login',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Center(
+            child: Text(
+              'Or, login with...',
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              SignInMethodWidget(
+                path: 'assets/google.png',
+              ),
+              SignInMethodWidget(
+                path: 'assets/apple.png',
+              ),
+              SignInMethodWidget(
+                path: 'assets/facebook.png',
+              ),
+            ],
+          ),
+         const SizedBox(height: 16,),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('New to iThickLogisticks? '),
+                GestureDetector(
+                  onTap: () {
+                    context.go('/register');
+                  },
+                  child: const Text(
+                    'Register',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                      color: Colors.blue
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class SignInMethodWidget extends StatelessWidget {
+  const SignInMethodWidget({
+    Key? key,
+    required this.path,
+  }) : super(key: key);
+  final String path;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        border: Border.all(
+          color: Colors.grey,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(
+            16,
+          ),
+        ),
+      ),
+      height: 60,
+      width: 60,
+      child: Image.asset(
+        path,
+        width: 20,
+        height: 20,
       ),
     );
   }
