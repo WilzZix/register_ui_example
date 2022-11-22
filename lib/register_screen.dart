3542301fe87eb520d06097643b90e4a37263cbf1
nodir <nodirbarotov0707@gmail.com>
2022-11-22 10:16:00 +0500

    nothing

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import 'main.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -10,6 +13,106 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          Image.asset(
+            'assets/register.jpg',
+            height: 400,
+            width: 400,
+          ),
+          const Text(
+            'Sign up',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              SignInMethodWidget(
+                path: 'assets/google.png',
+              ),
+              SignInMethodWidget(
+                path: 'assets/apple.png',
+              ),
+              SignInMethodWidget(
+                path: 'assets/facebook.png',
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Center(
+            child: Text(
+              'Or, register with email...',
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Email',
+              prefixIcon: Icon(
+                Icons.mail,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Password',
+              prefixIcon: Icon(
+                Icons.lock,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Full name',
+              prefixIcon: Icon(
+                Icons.lock,
+              ),
+            ),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Company name',
+              prefixIcon: Icon(
+                Icons.lock,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+            child: TextButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue)),
+              child: const Text(
+                'Sign up',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+        ],
+      ),
+    );
   }
 }
